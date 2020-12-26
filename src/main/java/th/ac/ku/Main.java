df1939e9b59c4af7a32bcf38c0245b4aa6907d7a
Aut_Atawee <autist_1210@hotmail.com>
2020-12-26 11:55:09 +0700

    Initial commit for Spring Annotation Based

---
+++ src/main/java/th/ac/ku/Main.java
@@ -0,0 +1,46 @@
+package th.ac.ku;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean.xml") ;
+        AtmUI atmUI = context.getBean(AtmUI.class) ;
+        atmUI.run();
+
+
+        //Normal
+//        Atm atm = context.getBean(Atm.class) ;
+
+//        String name = atm.validateCustomer(2, 2345) ;
+//        System.out.println( name + " has " + atm.getBalance() );
+//        atm.withdraw(200);
+//        System.out.println( name + " has " + atm.getBalance() );
+//
+//
+//        Bank bank = context.getBean(Bank.class) ;
+//        System.out.println(bank.getName());
+
+
+
+//        DataSourceDB dataSourceDB = new DataSourceDB() ;
+//        DataSourceFile dataSourceFile = new DataSourceFile() ;
+//
+//        Bank myBank = new Bank("My Bank", new DataSourceWebAPI()) ;
+//
+//        //Bank yourBank = new Bank("Your Bank", dataSourceFile) ;
+
+
+//        //การส่ง dependency ของ bank ให้ Atm
+//        Atm atm = new Atm(myBank) ;
+//
+//        String name = atm.validateCustomer(2, 2345) ;
+//        System.out.println( name + " has " + atm.getBalance() );
+//        atm.withdraw(200);
+//        System.out.println( name + " has " + atm.getBalance() );
+    }
+}
